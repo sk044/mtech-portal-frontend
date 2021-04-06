c51e85d6cb8799719ed7b727786231d806f19892
Shubham Kumar <54908788+sk044@users.noreply.github.com>
2021-04-07 00:24:27 +0530

    -7

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -76,7 +76,7 @@ class Signup extends Component{
                                     <Button.Group size='large' className="mb-5 StudentOrEmployerTogggle">
                                         <Button onClick={this.toggleToStudent} className={this.state.sButtonActive=== true ? 'activeClass':'normalClass'}>Candidate</Button>
                                         
-                                        <Button onClick={this.toggleToEmployer} className={this.state.eButtonActive === true ? 'activeClass':'normalClass'}>Faculty</Button>
+                                        <Button onClick={this.toggleToEmployer} className={this.state.eButtonActive === true ? 'activeClass':'normalClass'}>Admin</Button>
                                     </Button.Group>
                                     {this.displaycomp()}
                                     </div>
