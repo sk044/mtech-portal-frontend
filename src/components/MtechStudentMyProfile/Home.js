55c3d2d29d1b7ca7737ecfe4596d2051d49c05f3
Umang Jain <jnumang1999@gmail.com>
2021-04-07 13:41:23 +0530

    on loading making buttons hidden

--- src/components/MtechStudentMyProfile/Home.js
+++ src/components/MtechStudentMyProfile/Home.js
@@ -15,7 +15,7 @@ export default function Home(props) {
 
     if(props.data!==undefined){
         var str2 = props.data===null?null:props.data.gateScoreCard;
-        if(document.getElementById("ad8") !== null){
+        if(document.getElementById("ad8") !== null && document.getElementById('ugsc') !== null){
         if(str2 != null){
             var ad8 = document.getElementById("ad8");
             ad8.innerHTML="Uploaded";
@@ -33,6 +33,10 @@ export default function Home(props) {
 
         //for status of application
         var str = props.data?null:props.data.personalInfoUploadStatus.toString(); 
+        document.getElementById('edfi').style.visibility = 'visible';
+        document.getElementById('ugsc').style.visibility = 'visible';
+        document.getElementById('upp').style.visibility = 'visible';
+            
         var edfi = document.getElementById("edfi");
 
         console.log(str);
@@ -70,6 +74,18 @@ export default function Home(props) {
         }
     }
     }
+    else
+    {
+        if(document.getElementById("edfi") !== null){
+            document.getElementById('edfi').style.visibility = 'hidden';
+        }
+        if(document.getElementById("ugsc") !== null){
+            document.getElementById('ugsc').style.visibility = 'hidden';
+        }
+        if(document.getElementById("upp") !== null){
+            document.getElementById('upp').style.visibility = 'hidden';
+        }
+    }
 
     
     return (
@@ -84,16 +100,16 @@ export default function Home(props) {
                         <div className="name">
                             <h1>Welcome , {!props.data?null:props.data.name}</h1>
                             <h3>
-                            <button className="mtech_btn" id="edfi" onClick ={()=>{window.location.href = "/mtechstuinfo/"+props.data._id}} data={props.data}>Fill Basic Application Form</button>
-                            <button className="mtech_btn" onClick ={()=>{window.location.href = "/uploadgate/"+props.data._id}}>Upload Gate Score Card</button>
-                            <button className="mtech_btn" onClick ={()=>{window.location.href = "/uploadprofilepic/"+props.data._id}}>Upload Profile Pic</button>                           
+                            <button className="mtech_btn" id="edfi" onClick ={()=>{window.location.href = "/mtechstuinfo/"+props.data._id}} data={props.data} style={{}}>Fill Basic Application Form</button>
+                            <button className="mtech_btn" id="ugsc" onClick ={()=>{window.location.href = "/uploadgate/"+props.data._id}} style={{}}>Upload Gate Score Card</button>
+                            <button className="mtech_btn" id="upp" onClick ={()=>{window.location.href = "/uploadprofilepic/"+props.data._id} style={{}}}>Upload Profile Pic</button>                           
                             </h3>                              
                         </div>     
                     </div>
                 </div>
                 <div className="details">
                 <p id="applyhelper" style={{color: "blue" , paddingLeft:"20px"}}></p>
-                <button className="mtech_btn" id="apply" onClick ={()=>{window.location.href = "/applymore/"+props.data._id}} style={{background:'Salmon',color:"black"}}>Apply</button>
+                <button className="mtech_btn" id="apply" onClick ={()=>{window.location.href = "/applymore/"+props.data._id}} style={{background:'Salmon',color:"black", visibility:'hidden'}}>Apply</button>
                     <div className="status">
                         <h4>Filled Forms</h4>
                         <br/>
